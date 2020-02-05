4030f35719732ee330434b322a3711e5ac2ccc5b
John <johnopana2016@gmail.com>
2020-02-05 09:28:45 +0300

    added admin.py

--- photo/admin.py
+++ photo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from . models import Picture, Category, Location
+
+
+
+admin.site.register(Picture)
+admin.site.register(Category)
+admin.site.register(Location)
+
+

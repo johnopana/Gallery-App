e90942cab85d34dd179ba76aa42ac9e06605ca46
John <johnopana2016@gmail.com>
2020-02-05 09:31:39 +0300

    added test.py

--- photo/urls.py
+++ photo/urls.py
@@ -1,72 +1,16 @@
-from django.test import TestCase
-from .models import Location,Picture,Category
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
-# Create your tests here.    
-class CategoryTestClass(TestCase):
-    def setUp(self):
-        '''
-        Method to be run in every beginning of the test
-        '''
-        self.sports= Category(category='sport')
+urlpatterns=[
+    path('',views.index,name = 'index'),
+    path('about/', views.about, name='about'),
+    path('search/', views.search_results, name='search_results'),
+    path('image/<int:image_id>', views.view_image,name='view_image'),
+    path('category/<int:id>', views.category,name='category')
 
-    def test_instance(self):
-        self.assertTrue(isinstance(self.sports,Category))
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
-    def tearDown(self):
-        '''
-        Method to clear the test that has been done on category
-        '''
-        Category.objects.all().delete()
-
-    def test_save_method(self):
-        '''
-        Method to save category
-        
-        '''
-        self.sports.save_category()
-        category = Category.objects.all()
-        self.assertTrue(len(category)>0)
-
-    def test_delete_method(self):
-        '''
-        Method to delete the category
-        '''
-        self.sports.delete_category('sports')
-        category = Category.objects.all()
-        self.assertTrue(len(category)==0)
-
-class LocationTestClass(TestCase):
-
-    #set up method
-    def setUp(self):
-        '''
-        Method to be run in every beginning of the test
-        '''
-        self.nairobi= Location(location='nairobi')
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.nairobi,Location))
-
-    def tearDown(self):
-        '''
-        Method to clear the test that has been done on location
-        '''
-        Location.objects.all().delete()
-
-    def test_save_method(self):
-        '''
-        Method to save the location
-        
-        '''
-        self.nairobi.save_location()
-        locations = Location.objects.all()
-        self.assertTrue(len(locations)>0)
-
-    def test_delete_method(self):
-        '''
-        Method to delete the location
-        
-        '''
-        self.nairobi.delete_location('nairobi')
-        locations = Location.objects.all()
-        self.assertTrue(len(locations)==0)
